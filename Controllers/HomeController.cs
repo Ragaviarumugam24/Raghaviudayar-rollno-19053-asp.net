9ee4db41156d3767aa923f6234d39f35cc3372f8
Ragaviarumugam24 <ragaviudayar23@gmail.com>
2025-11-29 20:24:49 +0530

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,18 +1,9 @@
-using System.Diagnostics;
-using HotelApp.Models;
 using Microsoft.AspNetCore.Mvc;
 
-using HotelApp.Controllers;
+namespace HotelApp.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
@@ -22,11 +13,6 @@ using HotelApp.Controllers;
         {
             return View();
         }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
+
